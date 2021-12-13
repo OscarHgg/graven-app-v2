ca3b449a4138341189d850cbbb18e26543bdd8ef
Oscar Hägglund <oscarhagglund@live.com>
2021-12-13 19:20:54 +0100

    simplified calendar screen

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gravenv2_app/widgets/graven_calendar.dart';
 
-class CalendarScreen extends StatefulWidget {
+class CalendarScreen extends StatelessWidget {
   const CalendarScreen({Key? key}) : super(key: key);
 
   @override
-  _CalendarScreenState createState() => _CalendarScreenState();
-}
-
-class _CalendarScreenState extends State<CalendarScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GravenCalendar(),
-    );
-  }
+  Widget build(BuildContext context) => const Scaffold(
+        body: GravenCalendar(),
+      );
 }
