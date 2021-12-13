77a1d0705eeef3fefd03b3302587dc3c58f6b9e0
Oscar Hägglund <oscarhagglund@live.com>
2021-12-13 19:21:04 +0100

    set up chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:gravenv2_app/widgets/chat/messages.dart';
+import 'package:gravenv2_app/widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const Scaffold(
-          body: Center(
-        child: Text('ChatScreen'),
+  Widget build(BuildContext context) => Scaffold(
+          body: Column(
+        children: const [
+          Expanded(
+            child: Messages(),
+          ),
+          NewMessage(),
+        ],
       ));
 }
